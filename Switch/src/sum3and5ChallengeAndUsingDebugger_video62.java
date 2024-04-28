573e57b8ed8ffdbb8c673022fadedc6153762e9a
Andrii S <testengineer969@gmail.com>
2024-04-28 22:01:01 +0300

    update

--- Switch/src/sum3and5ChallengeAndUsingDebugger_video62.java
+++ Switch/src/sum3and5ChallengeAndUsingDebugger_video62.java
@@ -2,16 +2,16 @@ public class sum3and5ChallengeAndUsingDebugger_video62 {
 
     public static void main(String[] args) {
 
-        int totalSum = 0;
-        int numbersMetConditions = 0;
+        int sumOfMatches = 0;
+        int countOfMatches = 0;
 
         for (int i = 1; i <= 1000; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("This number meets conditions: " + i);
-                numbersMetConditions++;
-                totalSum = totalSum + i;
-                if (numbersMetConditions == 5) {
-                    System.out.println("Total Sum is " + totalSum);
+                countOfMatches++;
+                sumOfMatches += i;
+                if (countOfMatches == 5) {
+                    System.out.println("Total Sum is " + sumOfMatches);
                     break;
                 }
             }
