3383199fc1223e5ba87b3ee8193cb9690cfca978
Andrii S <testengineer969@gmail.com>
2024-05-11 21:19:25 +0300

    update

--- ControlFlow/src/codingExercise_22.java
+++ ControlFlow/src/codingExercise_22.java
@@ -6,6 +6,15 @@ public class codingExercise_22 {
 
     }
 
+
+    // Greatest Common Divisor
+    // Write a method named getGreatestCommonDivisor with two parameters
+    // of type int named first and second.
+    // If one of the parameters is < 10, the method should return -1 to indicate an invalid value.
+    // The method should return the greatest common divisor of the two numbers (int).
+    // The greatest common divisor is the largest positive integer that can fully divide
+    // each of the integers (i.e. without leaving a remainder).
+
     public static int getGreatestCommonDivisor(int a, int b) {
         if (a < 10 || b < 10) {
            return -1;
