1940cfd48a486e6428a5533123380ed9cd4dd780
Andrii Sav <testengineer969@gmail.com>
2024-08-14 22:32:35 +0300

    daily progress

--- List_ArrayList_LinkedList_Iterator_Autoboxing/src/video140_linkedListChallenge.java
+++ List_ArrayList_LinkedList_Iterator_Autoboxing/src/video140_linkedListChallenge.java
@@ -4,21 +4,30 @@ import java.util.Scanner;
 
 public class video140_linkedListChallenge {
 
-    record Place(String town, int distanceFromSydney) {}
+    record Place(String town, int distanceFromSydney) {
+        @Override
+        public String toString() {
+            return String.format("%s (%d)", town, distanceFromSydney);
+        }
+    }
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         LinkedList<Place> placesToVisit = new LinkedList<>();
-        placesToVisit.add(new Place("Melbourne", 1374));
-        placesToVisit.add(new Place("Brisbane", 917));
-        placesToVisit.add(new Place("Adelaide", 1374));
-        placesToVisit.add(new Place("Alice Springs", 2771));
-        placesToVisit.add(new Place("Perth", 3923));
+        placesToVisit.add(new Place("Brisbane", 500));
+        placesToVisit.add(new Place("Adelaide", 1000));
+        placesToVisit.add(new Place("Alice Springs", 1100));
+        placesToVisit.add(new Place("Perth", 1300));
+        Place melbourne = new Place("Melbourne", 1400);
+        addPlace(placesToVisit, melbourne);
+//        addPlace(placesToVisit, new Place("Melbourne", 1233));// duplicate (isn't added)
+        addPlace(placesToVisit, new Place("Lviv", 1232));
+
+        System.out.println(placesToVisit);
 
         System.out.println("Here is the list of places to visit :" + placesToVisit);
-//        printOptions();
-
         ListIterator<Place> iterator = placesToVisit.listIterator();
         boolean notQuit = true;
         while (notQuit) {
@@ -47,7 +56,7 @@ public class video140_linkedListChallenge {
         }
     }
 
-    public static void printOptions() {
+    private static void printOptions() {
         System.out.println("""
                 Available actions (select letter):
                 (F)orward
@@ -56,5 +65,46 @@ public class video140_linkedListChallenge {
                 (M)enu
                 (Q)uit
                 """);
+    }
+
+    public static void addPlace(LinkedList<Place> myList, Place place) {
+
+        // This option just checks the name of the Place type
+        if (myList.contains(place)) {
+            System.out.println("Found duplicate. There is such a place.");
+            return;
+        }
+
+
+        // This option is good (it's working)
+//        ListIterator<Place> iterator = myList.listIterator();
+//        while (iterator.hasNext()) {
+//            if (iterator.next().town().equalsIgnoreCase(place.town())) {
+//                System.out.println("Found duplicate. There is such a place.");
+//                return;
+//            }
+//        }
+//        myList.add(place);
+
+        // Enhanced version
+        for (Place value : myList) {
+            if (value.town().equalsIgnoreCase(place.town())) {
+                System.out.println("Found duplicate. There is such a place.");
+                return;
+            }
+        }
+//        myList.add(place);
+
+        int currentIndex = 0;
+        for (var value : myList) {
+            if (place.distanceFromSydney() < value.distanceFromSydney) {
+                myList.add(currentIndex, place); // adding the place that has less distance than the compared place,
+                                                // and adding a new place on the same index (so old place is pushed)
+                return;
+            }
+            currentIndex++;
+        }
+        myList.add(place);
+
     }
 }
