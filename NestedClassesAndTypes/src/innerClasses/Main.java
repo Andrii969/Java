7f3b39cacb9fae61d0ddd071920a4fefba3c8985
Andrii Sav <testengineer969@gmail.com>
2024-09-11 22:50:51 +0300

    daily progress

--- NestedClassesAndTypes/src/innerClasses/Main.java
+++ NestedClassesAndTypes/src/innerClasses/Main.java
@@ -30,7 +30,27 @@ public class Main {
             System.out.println(employee);
         }
 
+//        Employee testEmployee1 = new Employee(5, "Test1", 2323);
+//        Employee testEmployee2 = new Employee(6, "Test2", 2323);
+//        var comparator = new Employee.EmployeeComparator<>().compare(testEmployee1, testEmployee2);
+//        System.out.println(comparator); // -1
 
+        System.out.println("Store Members");
+        List<StoreEmployee> storeEmployees = new ArrayList<>(List.of(
+                new StoreEmployee(5, "Meg", 2019, "Target"),
+                new StoreEmployee(7, "Joe", 2018, "Walmart"),
+                new StoreEmployee(6, "Tom", 2021, "Macys"),
+                new StoreEmployee(8, "Marty", 2020, "Walmart")));
+
+        // !!!
+//        var comparator = new StoreEmployee.StoreComparator<>();// ERROR 'innerClasses.StoreEmployee' is not an enclosing class
+        var genericStoreEmployee = new StoreEmployee();
+        var comparator = genericStoreEmployee.new StoreComparator<>(); // alternatively, new StoreEmployee().new StoreComparator<>();
+        storeEmployees.sort(comparator); // sorted firstly by store, then by the yearStarted
+
+        for (StoreEmployee e : storeEmployees) {
+            System.out.println(e);
+        }
 
     }
 }
